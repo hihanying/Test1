493631bdb76d824100e74f83d917ea6b6aeee3e9
hihanying <hihanying@gmail.com>
2020-09-08 15:35:37 +0800

    version 1

---
+++ HelloMyGit/src/main/java/com/hihanying/git/HelloMyGit.java
@@ -0,0 +1,8 @@
+package com.hihanying.git;
+
+public class HelloMyGit {
+    public static void main(String[] args) {
+        System.out.println("Hello My Git!");
+        System.out.println("version 1");
+    }
+}
