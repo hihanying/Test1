d0b72372b453e5fbd5d9531c4b17600dadcebd44
hihanying <hihanying@gmail.com>
2020-09-08 15:36:09 +0800

    version 3

--- HelloMyGit/src/main/java/com/hihanying/git/HelloMyGit.java
+++ HelloMyGit/src/main/java/com/hihanying/git/HelloMyGit.java
@@ -4,5 +4,7 @@ public class HelloMyGit {
     public static void main(String[] args) {
         System.out.println("Hello My Git!");
         System.out.println("version 1");
+        System.out.println("version 2");
+        System.out.println("version 3");
     }
 }
